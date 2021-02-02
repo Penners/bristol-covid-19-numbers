b6f0b4995ec555e2db63bf307f4739cf85c0acf1
Penry Downey <penry.downey@apsleygroup.co.uk>
2021-02-02 23:01:44 +0000

    change to use static props and refactor

--- components/graph.js
+++ components/graph.js
@@ -1,52 +1,55 @@
 import Chart from "react-google-charts";
-import prepGraphData from "../lib/prepGraphData";
 
-const graph = (props) => {
-  const data = prepGraphData(
-    props.dataSet,
-    ["", props.hoverLabel],
-    props.totalCases
-  );
-  const options = {
-    chart: {},
-    animation: {
-      startup: true,
-      duration: 1,
-    },
-    legend: { position: "none" },
-  };
-  return (
-    <React.Fragment>
-      <div className="aspect-ratio-box">
-        <div className="aspect-ratio-box-inside">
-          <Chart
-            width={"100%"}
-            height={"100%"}
-            chartType="Line"
-            loader={<div>Loading Chart</div>}
-            data={data}
-            options={options}
-          />
-        </div>
-      </div>
-      <style jsx>{`
-        .aspect-ratio-box {
-          height: 0;
-          overflow: hidden;
-          padding-top: calc(1080 / 1920 * 100%);
-          background: white;
-          position: relative;
-        }
-        .aspect-ratio-box-inside {
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-        }
-      `}</style>
-    </React.Fragment>
-  );
+const graph = ({dataSet}) => {
+
+    const options = {
+        chart: {},
+        animation: {
+            startup: true,
+            duration: 3,
+        },
+        legend: { position: "none" },
+    };
+    return (
+        <>
+            <div className="aspect-ratio-box">
+                <div className="aspect-ratio-box-inside">
+                    <Chart
+                        width={"100%"}
+                        height={"100%"}
+                        chartType="Line"
+                        loader={<div>Loading Chart</div>}
+                        data={dataSet}
+                        options={options}
+                    />
+                </div>
+            </div>
+            <style jsx>
+            {`
+                .aspect-ratio-box {
+                    height: 0;
+                    overflow: hidden;
+                    padding-top: calc(1440 / 1920 * 100%);
+                    background: white;
+                    position: relative;
+                }
+                .aspect-ratio-box-inside {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                }
+
+                @media(min-width: 480px){
+                    .aspect-ratio-box {
+                        padding-top: calc(1080 / 1920 * 100%);
+                    }
+                }
+            `}
+            </style>
+        </>
+    );
 };
 
 export default graph;
